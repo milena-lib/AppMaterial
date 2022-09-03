e3102bfc39cfad219cb2ec11fe52192d1ed6b715
milena-lib <57179624+milena-lib@users.noreply.github.com>
2022-09-03 22:32:00 +0300

    fix setTimeout for highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -69,7 +69,7 @@ import { DragAndDrop1Component } from './drag-and-drop1/drag-and-drop1.component
 import { DragAndDrop2Component } from './drag-and-drop2/drag-and-drop2.component';
 import { ArraySortPipe, ArrayFilterPipe  } from './drag-and-drop2/array.pipe';
 import { DragDropDualListComponent } from './drag-and-drop2/drag-drop-dual-list/drag-drop-dual-list.component';
-import { HighlightDirective } from './directives/app-highlight.directive';
+import { HighlightDirective } from './directives/highlight.directive';
 
 const modules = [
   ReactiveFormsModule,
