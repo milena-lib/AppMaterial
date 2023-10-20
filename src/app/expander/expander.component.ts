a04f24ac33442971b765bd4c7d13c9295bf6cbba
milena-lib <milena.liberman@gmail.com>
2023-10-21 01:04:42 +0300

    expander css

--- src/app/expander/expander.component.ts
+++ src/app/expander/expander.component.ts
@@ -6,11 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expander.component.scss']
 })
 export class ExpanderComponent implements OnInit {
-  shouldOpenPanel1 = false;
+  shouldOpenPanel1 = true;
+  shouldOpenChildPanel1 = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClickHeader($event) {
+    console.log("onClickHeader: ", $event);
+    console.log("shouldOpenChildPanel1: ", this.shouldOpenChildPanel1);
+  }
+
+  openGroup($event) {
+    this.shouldOpenChildPanel1 = true;
+    console.log("openGroup: ", $event);
+  }
+
+  closedGroup($event) {
+    this.shouldOpenChildPanel1 = false;
+    console.log("closedGroup: ", $event);
   }
 
 }
