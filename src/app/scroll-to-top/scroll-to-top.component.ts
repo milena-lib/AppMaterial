4c5bb88398e07ce8008fcf3a13b7ecdbe9ac9478
milena-lib <milena.liberman@gmail.com>
2023-10-21 18:02:58 +0300

    scroll-to-top

--- src/app/scroll-to-top/scroll-to-top.component.ts
+++ src/app/scroll-to-top/scroll-to-top.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-to-top',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scroll-to-top.component.scss']
 })
 export class ScrollToTopComponent implements OnInit {
+  windowScrolled: boolean;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener("window:scroll", [])
+
+  onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    } 
+    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) { 
+      this.windowScrolled = false;
+    }
+  } 
 
   ngOnInit(): void {
   }
